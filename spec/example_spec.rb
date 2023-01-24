317d8bbc4e9c984001ead46eeff22f94cac55a93
ppp <ppp.3ppj@gmail.com>
2023-01-25 00:29:52 +0700

    rubocop and simplecov

---
+++ spec/example_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Test Testing' do
+  describe 'eq' do
+    it '45+9' do
+      expect(45 + 9).to eq(54)
+    end
+  end
+end
