582e8a19ff0e9ed4a1b70b8b2281933c51f08111
ppp <ppp.3ppj@gmail.com>
2023-01-25 19:43:42 +0700

    auth token and jwt

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,18 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :confirmable
+
+  before_validation :generate_auth_token, on: [:create]
+  
+  def generate_auth_token(force = false) 
+    self.auth_token ||= SecureRandom.urlsafe_base64
+
+    sef.auth_token = SecureRandom.urlsafe_base64 if force
+  end
+
+  def jwt(exp = 1.days.from_now)
+    payload = { exp: exp.to_i, auth_token: self.auth_token }
+    JWT.encode payload, Rails.application.credentials.secret_key_base, 'HS256'
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_25_122936) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_25_124045) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -36,6 +36,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_25_122936) do
     t.datetime "updated_at", null: false
     t.text "first_name"
     t.text "last_name"
+    t.text "auth_token"
+    t.index ["auth_token"], name: "index_users_on_auth_token", unique: true
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
